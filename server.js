d07ac4b96a1180ad5086b49d40b54ec5a6f8eadb
tomleah <thomas@leahfamily.plus.com>
2018-02-13 17:18:57 +0000

    Serves '/data/recipes' for all recipe data

--- server.js
+++ server.js
@@ -3,9 +3,15 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+const db = require('./database/database-model-sql');
+
 app.use('/', express.static('static'));
 
-app.listen(PORT, (err) => {
+app.get('/data/recipes', async (req, res) => {
+  res.send(await db.getRecipes());
+});
+
+app.listen(PORT, async (err) => {
   if (err) console.error(err);
   else console.log(`Server running on PORT: ${PORT}`);
 });
