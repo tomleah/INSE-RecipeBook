e01d20fc53bed016088bae4299ebf4d71f9fb1c2
tomleah <thomas@leahfamily.plus.com>
2018-02-13 23:24:22 +0000

    Added the ability to populate the recipe template with data dynamically.

--- static/scripts/recipes.js
+++ static/scripts/recipes.js
@@ -1,13 +1,31 @@
-async function loadRecipes(){
+async function populateRecipes(){
+  const recipes = await getRecipes();
+
+  recipes.forEach((recipe) => {
+    const prefab = window.template_card.content.cloneNode(true);
+
+    const title = prefab.querySelector('#recipe_name');
+    title.textContent = recipe.recipe_name;
+
+    const description = prefab.querySelector('#recipe_description');
+    description.textContent = recipe.recipe_description;
+    //
+    const prep = prefab.querySelector('#recipe_time');
+    prep.textContent = recipe.recipe_prep_time + recipe.recipe_cook_time;
+
+    window.recipe_cards.appendChild(prefab);
+  });
+}
+
+async function getRecipes(){
   const response = await fetch('/data/recipes');
   if (!response.ok) {
     alert('error');
     console.log(response.status);
     return;
   }
-  const data = await response.json();
-  console.log(data);
-  return false;
+  const recipes = await response.json();
+  return recipes;
 }
 
-window.addEventListener('load', loadRecipes);
+window.addEventListener('load', populateRecipes);
