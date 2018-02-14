da7336f7915274cba6e80e860db152e437af7fbb
tomleah <thomas@leahfamily.plus.com>
2018-02-14 15:04:57 +0000

    Prepping for multiple pages of recipes.

--- database/init-database.sql
+++ database/init-database.sql
@@ -15,3 +15,9 @@ create table if not exists recipebook.Recipes (
 insert ignore into recipebook.Recipes values (1, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20, 4);
 insert ignore into recipebook.Recipes values (2, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 0);
 insert ignore into recipebook.Recipes values (3, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1);
+insert ignore into recipebook.Recipes values (4, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20, 4);
+insert ignore into recipebook.Recipes values (5, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 0);
+insert ignore into recipebook.Recipes values (6, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1);
+insert ignore into recipebook.Recipes values (7, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20, 4);
+insert ignore into recipebook.Recipes values (8, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 0);
+insert ignore into recipebook.Recipes values (9, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1);
