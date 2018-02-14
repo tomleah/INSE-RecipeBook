b3c467fe726cc03684c8c745e2bcfe65fd4f455f
tomleah <thomas@leahfamily.plus.com>
2018-02-14 13:44:09 +0000

    Updated database table for recipe_feeds

--- database/init-database.sql
+++ database/init-database.sql
@@ -6,9 +6,10 @@ create table if not exists recipebook.Recipes (
   recipe_description text,
   recipe_img_name varchar(100),
   recipe_prep_time int,
-  recipe_cook_time int
+  recipe_cook_time int,
+  recipe_feeds int
 );
 
-insert ignore into recipebook.Recipes values (1, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20);
-insert ignore into recipebook.Recipes values (2, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones may are well cooked.', 'fishcakes2.png', 30, 120);
-insert ignore into recipebook.Recipes values (3, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20);
+insert ignore into recipebook.Recipes values (1, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20, 4);
+insert ignore into recipebook.Recipes values (2, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 0);
+insert ignore into recipebook.Recipes values (3, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1);
