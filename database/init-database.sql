8995a7de2cb053ceeda0d7869540073cff048505
Jesus Vaquerizo <jvaquerizoplumed@gmail.com>
2018-03-09 21:57:10 +0000

    Added new complete recipe
    Added a new recipe to the database

--- database/init-database.sql
+++ database/init-database.sql
@@ -18,7 +18,7 @@ create table recipebook.Recipes(
 insert into recipebook.Recipes values (null, 'Fish Cakes', 'A fish cakes recipe from england.', 'fishcakes1.png', 30, 20, 4, 0);
 insert into recipebook.Recipes values (null, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 2, 0);
 insert into recipebook.Recipes values (null, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1, 0);
-insert into recipebook.Recipes values (null, 'Vegeterian Pizza', 'Delicious italian vegerarian pizza.', 'cardcover001.png', 40, 15, 2, 1);
+insert into recipebook.Recipes values (null, 'Three Pepper Pizza Pizza', 'Delicious and healthy pizza.', 'cardcover001.png', 40, 15, 3, 1);
 insert into recipebook.Recipes values (null, 'Chocolate cake', 'Chocolate cake topped with berries', 'cardcover002.png', 40, 15, 2, 1);
 
 create table recipebook.RecipeMethod(
@@ -36,8 +36,11 @@ insert into recipebook.RecipeMethod values (null, 2, 'To prepare crispy fish cak
 insert into recipebook.RecipeMethod values (null, 2, 'Once you have the ingredients prepared let get cooking the crispy fish cakes');
 insert into recipebook.RecipeMethod values (null, 3, 'To prepare a cakes follow the following prepararion instructions...');
 insert into recipebook.RecipeMethod values (null, 3, 'Once you have the ingredients prepared let get cooking the cake');
-insert into recipebook.RecipeMethod values (null, 4, 'To prepare a vegerarian pizza you will need to pre heat the oven...');
-insert into recipebook.RecipeMethod values (null, 4, 'Once you have the ingredients ready, add them to the pizza and leave in the oven for 10 minutes...');
+insert into recipebook.RecipeMethod values (null, 4, 'Preheat oven to 230°C');
+insert into recipebook.RecipeMethod values (null, 4, 'Dice peppers, and chopp the onion');
+insert into recipebook.RecipeMethod values (null, 4, 'Combine seasoning, tomato paste, and water in a small bowl; stir well.');
+insert into recipebook.RecipeMethod values (null, 4, 'Spread the ingredients over pizza crust. Top evenly with cheese. Sprinkle bell pepper and onion evenly over cheese.');
+insert into recipebook.RecipeMethod values (null, 4, 'Bake at 230°C for 10 to 12 minutes or until cheese melts. Cut into 6 wedges, and serve.');
 insert into recipebook.RecipeMethod values (null, 5, 'To prepare the chocolate cake do the following prepararion instructions...');
 insert into recipebook.RecipeMethod values (null, 5, 'Once you have the ingredients prepared let get cooking the chocolate cake');
 
@@ -58,30 +61,40 @@ insert into recipebook.Ingredient values (null, 'Bell peppers');
 insert into recipebook.Ingredient values (null, 'Whipped cream');
 insert into recipebook.Ingredient values (null, 'Breadcrumbs');
 insert into recipebook.Ingredient values (null, 'Tomato sauce');
+insert into recipebook.Ingredient values (null, 'Italian seasoning');
+insert into recipebook.Ingredient values (null, 'Tomato paste');
+insert into recipebook.Ingredient values (null, 'Water');
+insert into recipebook.Ingredient values (null, 'Prebaked refrigerated pizza crust');
+insert into recipebook.Ingredient values (null, 'Mozzarella cheese');
+insert into recipebook.Ingredient values (null, 'Onion');
+
 
 create table recipebook.RecipeIngredients(
   recipe_id int,
   ingredient_id int,
-  recipeIngredients_quantity int,
+  recipeIngredients_quantity varchar(500),
 
   constraint primary key(recipe_id, ingredient_id),
   constraint foreign key(recipe_id) references Recipes(recipe_id),
   constraint foreign key(ingredient_id) references Ingredient(ingredient_id)
 );
 
-insert into recipebook.RecipeIngredients values (1, 9, 200);
-insert into recipebook.RecipeIngredients values (1, 5, 2);
-insert into recipebook.RecipeIngredients values (1, 4, 25);
-insert into recipebook.RecipeIngredients values (2, 9, 300);
-insert into recipebook.RecipeIngredients values (2, 5, 3);
-insert into recipebook.RecipeIngredients values (2, 4, 50);
-insert into recipebook.RecipeIngredients values (4, 10, 150);
-insert into recipebook.RecipeIngredients values (4, 7, 2);
-insert into recipebook.RecipeIngredients values (4, 6, 400);
-insert into recipebook.RecipeIngredients values (4, 5, 2);
-insert into recipebook.RecipeIngredients values (5, 1, 100);
-insert into recipebook.RecipeIngredients values (5, 3, 10);
-insert into recipebook.RecipeIngredients values (5, 4, 25);
-insert into recipebook.RecipeIngredients values (5, 5, 2);
-insert into recipebook.RecipeIngredients values (5, 6, 500);
-insert into recipebook.RecipeIngredients values (5, 8, 100);
+insert into recipebook.RecipeIngredients values (1, 9, '200');
+insert into recipebook.RecipeIngredients values (1, 5, '2');
+insert into recipebook.RecipeIngredients values (1, 4, '25');
+insert into recipebook.RecipeIngredients values (2, 9, '300');
+insert into recipebook.RecipeIngredients values (2, 5, '3');
+insert into recipebook.RecipeIngredients values (2, 4, '50');
+insert into recipebook.RecipeIngredients values (4, 11, '1/4 teaspoon');
+insert into recipebook.RecipeIngredients values (4, 12, '1/3 cup');
+insert into recipebook.RecipeIngredients values (4, 13, '1/4 cup');
+insert into recipebook.RecipeIngredients values (4, 14, '1 (12-inch)');
+insert into recipebook.RecipeIngredients values (4, 15, '1 cup (100 grams) shredded');
+insert into recipebook.RecipeIngredients values (4, 7, '1 1/2 cups, 3 small');
+insert into recipebook.RecipeIngredients values (4, 16, '1/2');
+insert into recipebook.RecipeIngredients values (5, 1, '100');
+insert into recipebook.RecipeIngredients values (5, 3, '10');
+insert into recipebook.RecipeIngredients values (5, 4, '25');
+insert into recipebook.RecipeIngredients values (5, 5, '2');
+insert into recipebook.RecipeIngredients values (5, 6, '500');
+insert into recipebook.RecipeIngredients values (5, 8, '100');
