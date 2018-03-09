c3b8bcb91cb82bc8ae05b4f20f24267ddccac8f0
Jesus Vaquerizo <UP820346@uni.ds.port.ac.uk>
2018-03-09 13:40:43 +0000

    Adden a new table containing the cooking instructions & some data
    Added a new table RecipeMethod that contains the cooking instructions.

--- database/init-database.sql
+++ database/init-database.sql
@@ -17,5 +17,20 @@ insert into recipebook.Recipes values (null, 'Fish Cakes', 'A fish cakes recipe 
 insert into recipebook.Recipes values (null, 'Crispy Fish Cakes', 'Another fish cakes recipe from england. These ones are well cooked.', 'fishcakes2.png', 30, 120, 2, 0);
 insert into recipebook.Recipes values (null, 'Cake', 'A cake recipe from france.', 'fishcakes3.png', 60, 20, 1, 0);
   --------------------------------
-insert into recipebook.Recipes values (null, 'Vegeterian Pizza', 'Delicious vegerarian pizza.', 'cardcover001.png', 40, 15, 2, 1);
+insert into recipebook.Recipes values (null, 'Vegeterian Pizza', 'Delicious italian vegerarian pizza.', 'cardcover001.png', 40, 15, 2, 1);
 insert into recipebook.Recipes values (null, 'Chocolate cake', 'Chocolate cake topped with berries', 'cardcover002.png', 40, 15, 2, 1);
+
+create table recipebook.RecipeMethod (
+  method_id int,
+  method_prep varchar(1000),
+  method_instr varchar(1000),
+
+  constraint primary key(method_id),
+  constraint foreign key(method_id) references Recipes(recipe_id)
+);
+
+insert into recipebook.RecipeMethod values (1, 'To prepare the english fish cakes follow the following prepararion instructions...', 'Once you have the ingredients prepared let get cooking the fish cakes');
+insert into recipebook.RecipeMethod values (2, 'To prepare crispy fish cakes follow the following prepararion instructions...', 'Once you have the ingredients prepared let get cooking the crispy fish cakes');
+insert into recipebook.RecipeMethod values (3, 'To prepare a cakes follow the following prepararion instructions...', 'Once you have the ingredients prepared let get cooking the cake');
+insert into recipebook.RecipeMethod values (4, 'To prepare a vegerarian pizza you will need to pre heat the oven...', 'Once you have the ingredients ready, add them to the pizza and leave in the oven for 10 minutes...');
+insert into recipebook.RecipeMethod values (5, 'To prepare the chocolate cake do the following prepararion instructions...', 'Once you have the ingredients prepared let get cooking the chocolate cake');
