67a18e59ecbe71eac96151bcc81964dea3a800fa
tomleah <thomas@leahfamily.plus.com>
2018-03-10 23:27:52 +0000

    Limited pictures client recieves instead of sending all of them at once.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const fs = require('fs');
+const config = require('./config');
+
 const PORT = process.env.PORT || 8080;
 
 const db = require('./database/database-model-sql');
@@ -22,18 +25,39 @@ app.listen(PORT, async (err) => {
 //Server functions
 const RECIPE_LIMIT = 4;
 async function sendRecipes(req, res){
-  // const recipeList = await db.getRecipes(req.query.search);
   const recipeList = (req.query.search) ? await db.searchRecipes(req.query.search) : await db.getRecipes();
   let page = Number(req.query.p) || 1;
   //How many pages of recipes are possible?
   const pageCount = Math.ceil(recipeList.length / RECIPE_LIMIT);
   if (page > pageCount) page = pageCount;
 
-  const recipes = recipeList.slice(RECIPE_LIMIT * (page - 1), RECIPE_LIMIT * page);
+  let recipes = recipeList.slice(RECIPE_LIMIT * (page - 1), RECIPE_LIMIT * page);
+  moveImages(recipes);
   res.send({
     recipes,
     page,
     pageCount
   });
 
+}
+
+function moveImages(recipes){
+  fs.readdir(config.clientimgpath, (err, items) => {
+    items.forEach((file) => {
+      fs.unlink(config.clientimgpath + file, (err) => {
+        if (err) console.error(err);
+      });
+    });
+  });
+
+  recipes.forEach((recipe, index) => {
+    fs.exists(config.serverimgpath + recipe.recipe_img_name, (exists) => {
+      if (exists) {
+        console.log(recipe.recipe_img_name + ' exists');
+        fs.createReadStream(config.serverimgpath + recipe.recipe_img_name).pipe(fs.createWriteStream(config.clientimgpath + recipe.recipe_img_name));
+      } else {
+        console.log(recipe.recipe_img_name + ' doesn\'t exist');
+      }
+    });
+  });
 }
