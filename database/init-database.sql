cb621abbc9c7cb972c615de082b2eef10bb46a23
tomleah <thomas@leahfamily.plus.com>
2018-03-11 18:00:25 +0000

    Altered database to be more suitable to query ingredients

--- database/init-database.sql
+++ database/init-database.sql
@@ -25,7 +25,7 @@ insert into Recipes values (null, 'Chocolate cake', 'Chocolate cake topped with 
 create table RecipeMethod(
   method_id int auto_increment,
   recipe_id int,
-  method_instr varchar(1000),
+  method_instr text,
 
   constraint primary key(method_id),
   constraint foreign key(recipe_id) references Recipes(recipe_id)
@@ -48,32 +48,33 @@ insert into RecipeMethod values (null, 5, 'Once you have the ingredients prepare
 create table Ingredient(
   ingredient_id int auto_increment,
   ingredient_name varchar(100),
+  ingredient_unit varchar(100) null,
 
   constraint primary key(ingredient_id)
 );
 
-insert into Ingredient values (null, 'Chocolate');
-insert into Ingredient values (null, 'Tomato');
-insert into Ingredient values (null, 'Berries');
-insert into Ingredient values (null, 'Oil');
-insert into Ingredient values (null, 'Eggs');
-insert into Ingredient values (null, 'Flour');
-insert into Ingredient values (null, 'Bell peppers');
-insert into Ingredient values (null, 'Whipped cream');
-insert into Ingredient values (null, 'Breadcrumbs');
-insert into Ingredient values (null, 'Tomato sauce');
-insert into Ingredient values (null, 'Italian seasoning');
-insert into Ingredient values (null, 'Tomato paste');
-insert into Ingredient values (null, 'Water');
-insert into Ingredient values (null, 'Prebaked refrigerated pizza crust');
-insert into Ingredient values (null, 'Mozzarella cheese');
-insert into Ingredient values (null, 'Onion');
+insert into Ingredient values (null, 'Chocolate', 'g');
+insert into Ingredient values (null, 'Tomato', null);
+insert into Ingredient values (null, 'Berries', 'g');
+insert into Ingredient values (null, 'Oil', 'ml');
+insert into Ingredient values (null, 'Eggs', null);
+insert into Ingredient values (null, 'Flour', 'g');
+insert into Ingredient values (null, 'Bell peppers', null);
+insert into Ingredient values (null, 'Whipped cream', null);
+insert into Ingredient values (null, 'Breadcrumbs', 'g');
+insert into Ingredient values (null, 'Tomato sauce', 'ml');
+insert into Ingredient values (null, 'Italian seasoning', 'g');
+insert into Ingredient values (null, 'Tomato paste', null);
+insert into Ingredient values (null, 'Water', 'ml');
+insert into Ingredient values (null, 'Prebaked refrigerated pizza crust', null);
+insert into Ingredient values (null, 'Mozzarella cheese', 'g');
+insert into Ingredient values (null, 'Onion', null);
 
 
 create table RecipeIngredients(
   recipe_id int,
   ingredient_id int,
-  recipeIngredients_quantity varchar(500),
+  recipeIngredients_quantity varchar(500) null,
 
   constraint primary key(recipe_id, ingredient_id),
   constraint foreign key(recipe_id) references Recipes(recipe_id),
@@ -86,16 +87,15 @@ insert into RecipeIngredients values (1, 4, '25');
 insert into RecipeIngredients values (2, 9, '300');
 insert into RecipeIngredients values (2, 5, '3');
 insert into RecipeIngredients values (2, 4, '50');
-insert into RecipeIngredients values (4, 11, '1/4 teaspoon');
-insert into RecipeIngredients values (4, 12, '1/3 cup');
-insert into RecipeIngredients values (4, 13, '1/4 cup');
-insert into RecipeIngredients values (4, 14, '1 (12-inch)');
-insert into RecipeIngredients values (4, 15, '1 cup (100 grams) shredded');
-insert into RecipeIngredients values (4, 7, '1 1/2 cups, 3 small');
+insert into RecipeIngredients values (4, 11, '10');
+insert into RecipeIngredients values (4, 12, null);
+insert into RecipeIngredients values (4, 14, '1');
+insert into RecipeIngredients values (4, 15, '50');
+insert into RecipeIngredients values (4, 7, '3');
 insert into RecipeIngredients values (4, 16, '1/2');
 insert into RecipeIngredients values (5, 1, '100');
 insert into RecipeIngredients values (5, 3, '10');
 insert into RecipeIngredients values (5, 4, '25');
 insert into RecipeIngredients values (5, 5, '2');
 insert into RecipeIngredients values (5, 6, '500');
-insert into RecipeIngredients values (5, 8, '100');
+insert into RecipeIngredients values (5, 8, null);
