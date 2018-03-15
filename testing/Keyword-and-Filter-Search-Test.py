431ed1339613d0cfbaf5b567867756030a5aab32
tomleah <thomas@leahfamily.plus.com>
2018-03-15 23:58:36 +0000

    Added a test script to test both filters and recipe keywords when used at the same time.

---
+++ testing/Keyword-and-Filter-Search-Test.py
@@ -0,0 +1,22 @@
+from helium.api import *
+import time
+
+url = input("Enter site(VM) URL/IP: ")
+start_chrome(url)
+
+click("New Filter")
+write("Eggs", S("#filter-name"))
+write("5", S("#filter-quantity"))
+click("Add")
+
+write("Chocolate", S("#recipe-search-query"))
+click(S("#recipe-search"))
+
+time.sleep(2)
+
+if len(find_all(S(".card-wrapper"))) == 1:
+    print("Test Passed")
+else:
+    print("Test Failed: An unexpected amount of recipes were produced")
+
+kill_browser()
